4b423fc47581958f0f4148bdb7486260df82a5b4
Takaso <70639611+Takaso@users.noreply.github.com>
2022-04-07 18:41:37 +0200

    Update senkrypt.py

--- senkrypt.py
+++ senkrypt.py
@@ -1,26 +1,35 @@
 import sys
 
 def crypt(text):
-    ewe = ""
+    if " " in text:
+      text.replace(" ", "〆")
+    else:
+      assert "〆" in text, "Don't try to break it!";
+    ewe = "";
     for sex in list(text):
-        e = ord(sex) * len(text)
-        ewe += hex(e)
-        ok = ewe.split("0x")
-    assert ok
-    print("".join(ok[::-1]))
+        e = ord(sex) * len(text);
+        ewe += hex(e);
+        ok = ewe.split("0x");
+    assert ok, "The text isn't valid.";
+    return "".join(ok[::-1]);
+
 
 def decrypt(text):
-    assert len(text)%3==0, "Invalid text"
-    first_step =  [text[i:i+3] for i in range(0, len(text), 3)]; second_step = []
-    for i in first_step:
-        second_step.append(int(i, 16))
-    second_step = second_step[::-1]; third_step = []
-    for i in second_step:
-        third_step.append(int(i/5))
-    final_step = []
-    for i in third_step:
-        final_step.append(chr(i))
-    print("".join([i for i in final_step]))
+  assert len(text)%3==0, "Invalid text";
+  first_step =  [text[i:i+3] for i in range(0, len(text), 3)]; second_step = [];
+  for i in first_step:
+    second_step.append(int(i, 16));
+  second_step = second_step[::-1]; third_step = [];
+  for i in second_step:
+    third_step.append(int(i/len(second_step)));
+  final_step = [];
+  for i in third_step:
+    final_step.append(chr(i));
+  gohan = "".join([i for i in final_step]);
+  if "〆" in gohan:
+    return gohan.replace("〆", " ");
+  else:
+    return gohan;
 
 
 if sys.argv[1].startswith("-c"):
